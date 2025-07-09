06a6618cd71eeff4a2be5e8cf3ebe9c82505eca0
gxy <1551705593@qq.com>
2025-07-09 13:17:08 +0800

    postcss 实现浏览器兼容和css代码压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import cssnano from 'cssnano'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css:{
+    postcss:{
+      plugins:[
+        autoprefixer({ // 自动添加前缀
+          overrideBrowserslist: [
+            "> 1%"   // 全球使用率大于1的浏览器
+          ],
+        }),
+        cssnano({
+          preset: 'default',
+        })
+      ]
+    }
+  }
 })
